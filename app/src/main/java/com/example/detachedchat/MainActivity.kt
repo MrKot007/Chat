02e5634cc5dd8383801658cf3039f1b29e713a27
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-04-07 10:48:12 +0300

    initialize

---
+++ app/src/main/java/com/example/detachedchat/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.detachedchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.detachedchat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+    }
+}
